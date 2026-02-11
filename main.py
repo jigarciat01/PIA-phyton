c8d564aaf079a686aa956279613d6075d0f8b930
jigarciat01 <jigarciat01@educarex.es>
2026-02-11 10:04:35 +0000

    nuevo main

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-print ("Bienvenido al software de gestión, por favor responde a las siguientes preguntas ")
+opcion=input("1_ Nombre completo, 2_Calculadora ")
 
-nombre = input("¿Cuál es tu nombre? ")
-apellido = input("¿Cuál es tu apellido? ")
-año_nacimiento = input("¿En qué año naciste? ")
-
-print ("Hola " + nombre + " " + apellido + ", naciste en el año " + año_nacimiento)
+if opcion=="1":
+    nombre=input("Ingrese su nombre: ")
+    apellido=input("Ingrese su apellido: ")
+    print("Su nombre completo es: ",nombre,apellido)
+elif opcion=="2":
+    num1=int(input("Ingrese el primer numero: "))
+    num2=int(input("Ingrese el segundo numero: "))
+    suma=num1+num2
+    resta=num1-num2
+    multiplicacion=num1*num2
+    division=num1/num2
+    print("La suma es: ",suma)
+    print("La resta es: ",resta)
+    print("La multiplicacion es: ",multiplicacion)
+    print("La division es: ",division)
+else:
+    print("Opcion no valida")   
