3d38eece3f0e8579288ca7f088f8f39769a9c9b8
jigarciat01 <jigarciat01@educarex.es>
2026-02-11 09:51:57 +0000

    nuevo main

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print ("Hola Mundo")
-print ("Quiero aprender python")
+print ("Bienvenido al software de gestión, por favor responde a las siguientes preguntas ")
+
+nombre = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+año_nacimiento = input("¿En qué año naciste? ")
+
+print ("Hola " + nombre + " " + apellido + ", naciste en el año " + año_nacimiento)
